5a5c0aa6578e23a5ccefc147a2615cf0d2406a47
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-08-05 00:21:17 +0400

    implement payment

---
+++ app/_data/PricingPlan.jsx
@@ -0,0 +1,14 @@
+export default [
+  {
+    link: "https://buy.stripe.com/test_3cs7sP1as67r2t28ww",
+    price: 5.5,
+    priceId: "price_1PkAKkRr8OxMjGLJTTu5osCu",
+    duration: "Monthly",
+  },
+  {
+    link: "https://buy.stripe.com/test_fZe28v9GY3Zj8Rq6op",
+    price: 55.5,
+    priceId: "price_1PkAYJRr8OxMjGLJYfqqvul9",
+    duration: "Yearly",
+  },
+];
